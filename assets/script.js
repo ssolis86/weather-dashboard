2f7e3dd4af49e07da8b535f87e27c3d2369a46b7
Stephen Solis <ssolis8619@gmail.com>
2021-02-18 21:23:10 -0600

    localstorage/ui updates

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,21 @@
+
+var sButton = document.querySelector('#searchButton');
+
+var formSubmitHandler = function(event) {
+  event.preventDefault;
+    var cityName = document.getElementById('userInput').value;
+    document.getElementById("cName").textContent = "City: " + cityName;
+    inputValue = cityName.trim();
+    
+    // getPetAPI(inputValue);
+		localStorage.setItem("city", inputValue);
+		var recentEntry = localStorage.getItem("city");
+		document.getElementById("cityOne").textContent = recentEntry;
+    
+  }
+
+
+
 fetch("http://api.openweathermap.org/data/2.5/weather?q=San Antonio,us&APPID=2a0ccef3d39a4025d5525f79d575070e", {
     cache: 'reload',
 })
@@ -6,6 +24,11 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=San Antonio,us&APPID=2a0
   })
   .then(function (data) {
     console.log(data);
+    var tempurature = "temp";
+    var humidity = 'humidity';
+
   });
+
+  sButton.addEventListener("click", formSubmitHandler);
 
 
