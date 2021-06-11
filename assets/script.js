1ad606ab6681d0b89d962710094c1aa67cfd01b8
ssolis1337 <71684140+ssolis86@users.noreply.github.com>
2021-06-10 21:23:50 -0500

    consoled logged data from api

--- assets/script.js
+++ assets/script.js
@@ -1,71 +1,82 @@
 
 var sButton = document.querySelector('#searchButton');
 var listOfCities = document.querySelector('#cityList');
+var usrInput = document.querySelector('#userInput');
 var recentSearches = [];
 
-var formSubmitHandler = function(event) {
-    event.preventDefault;
-    var cityName = document.getElementById('userInput').value;
-    document.getElementById("cName").textContent = "City: " + cityName;
-    inputValue = cityName.trim();
+var formSubmitHandler = function() {
+
+  fetch("http://api.openweathermap.org/data/2.5/weather?q=San Antonio,us&APPID=2a0ccef3d39a4025d5525f79d575070e", {
+    cache: 'reload',
     
-    if (localStorage.getItem('recentSearches') === null) {
+})
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    lat = (data.coord.lat);
+    lon = (data.coord.lon);
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=2a0ccef3d39a4025d5525f79d575070e`, {
+
+    })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (data2) {
+      console.log('current_weather', data.main.temp);
+      var wSpeed = data.wind.speed;
+      var humidity = data.main.humidity;
+      var tempurature = data.main.temp;
+      var faren = ((tempurature-273.15) * (9/5)) + 32
+      let fTemp = faren.toFixed(2);
+      let uvIndex = data2.current.uvi;
+      console.log("fTemp" + fTemp, "humidity" + humidity, "wind speed" + wSpeed, "UV index" + uvIndex );
       
-      recentSearches.push(inputValue);
-      localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-  
-    } else {
       
-      recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
-      recentSearches.push(inputValue);
-      localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
-      
-    }
+    })
+
     
-    renderCitiesSearched(localStorage.getItem('recentSearches'));
     
-  }
-  
+  });
+
+}
+    
+// I am presented with the 
+// city name, 
+// the date, 
+// an icon representation of 
+// weather conditions, 
+// the temperature, done
+// the humidity, done
+// the wind speed, 
+// and the UV index
+
 var renderCitiesSearched = function() {
-  
-  for (var i = 0; i < localStorage.getItem('recentSearches').length; i++) {
-    var cities = localStorage.getItem('recentSearches')[i];
+  listOfCities.innerHTML = "";
+
+  for (var i = 0; i < JSON.parse(localStorage.getItem("recentSearches")).length; i++) {
+    var cities = JSON.parse(localStorage.getItem("recentSearches"))[i];
 
     var li = document.createElement('li');
     li.textContent = cities;
+    li.setAttribute('data-index', i);
     console.log(cities);
     listOfCities.appendChild(li);
   }
+
 }
-
-
-
-fetch("http://api.openweathermap.org/data/2.5/weather?q=San Antonio,us&APPID=2a0ccef3d39a4025d5525f79d575070e", {
-    cache: 'reload',
-})
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('current_weather', data);
-    var tempurature = "temp";
-    var humidity = 'humidity';
-
-  });
-
-  fetch("http://api.openweathermap.org/data/2.5/forecast?q=San Antonio&appid=2a0ccef3d39a4025d5525f79d575070e", {
-    cache: 'reload',
-})
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('forecast',data);
-    var tempurature = "temp";
-    var humidity = 'humidity';
-
-  });
 
   sButton.addEventListener("click", formSubmitHandler);
 
+  // Execute a function when the user releases a key on the keyboard
+  usrInput.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+      event.preventDefault();
+    // Trigger the button element with a click
+    formSubmitHandler();
+    
+  }
+});
 
